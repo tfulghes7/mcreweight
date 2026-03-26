0908b73f15f1c10ebaa4b6599929a3c4e00360d7
tfulghes7 <tommyghesu@gmail.com>
2026-03-26 17:02:00 +0100

    Improve doc style

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.mathjax"]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 autodoc_member_order = "bysource"
